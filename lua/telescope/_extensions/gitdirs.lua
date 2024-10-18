a417bb686d1b601892a3ce2bd2670e4cdd1e112f
Charles Waldner <waldner.charles@gmail.com>
2024-10-18 12:13:52 -0600

    Move requires into search_git_dirs

--- lua/telescope/_extensions/gitdirs.lua
+++ lua/telescope/_extensions/gitdirs.lua
@@ -1,48 +1,42 @@
-local telescope = require 'telescope'
-local pickers = require 'telescope.pickers'
-local finders = require 'telescope.finders'
-local conf = require('telescope.config').values
-
 local function search_git_dirs(opts)
-    opts = opts or {}
+	opts = opts or {}
 
-    local search_dirs = {}
-    for i, d in ipairs(opts.search_dirs or { '~' }) do
-        search_dirs[i] = vim.fn.expand(d)
-    end
+	local search_dirs = {}
+	for i, d in ipairs(opts.search_dirs or { "~" }) do
+		search_dirs[i] = vim.fn.expand(d)
+	end
 
-    local exclude_dirs = {}
-    for i, d in ipairs(opts.exclude_dirs or { 'build', 'cmake-*' }) do
-        exclude_dirs[i] = '-E ' .. d
-    end
+	local exclude_dirs = {}
+	for i, d in ipairs(opts.exclude_dirs or { "build", "cmake-*" }) do
+		exclude_dirs[i] = "-E " .. d
+	end
 
-    local fd = { 'fd --case-sensitive --ignore-vcs --hidden --prune' }
+	local fd = { "fd --case-sensitive --ignore-vcs --hidden --prune" }
+	table.insert(fd, exclude_dirs)
+	table.insert(fd, { "--glob .git" })
+	table.insert(fd, search_dirs)
+	local fd_command = vim.iter(fd):flatten():join(" ")
 
-    table.insert(fd, exclude_dirs)
-    table.insert(fd, { '--glob .git' })
-    table.insert(fd, search_dirs)
-    local fd_command = vim.iter(fd):flatten():join ' '
+	print(fd_command)
 
-    print(fd_command)
+	local results = vim.fn.systemlist(fd_command) or {}
+	for i, result in ipairs(results) do
+		results[i] = result:gsub("%.git[\\/]$", "")
+	end
 
-    local results = vim.fn.systemlist(fd_command) or {}
-    for i, result in ipairs(results) do
-        results[i] = result:gsub('%.git[\\/]$', '')
-    end
-
-    pickers
-        .new(opts, {
-            prompt_title = 'Git Directories',
-            finder = finders.new_table {
-                results = results,
-            },
-            sorter = conf.generic_sorter(opts),
-        })
-        :find()
+	require("telescope.pickers")
+		.new(opts, {
+			prompt_title = "Git Directories",
+			finder = require("telescope.finders").new_table({
+				results = results,
+			}),
+			sorter = require("telescope.config").values.generic_sorter(opts),
+		})
+		:find()
 end
 
-return telescope.register_extension {
-    exports = {
-        gitdirs = search_git_dirs,
-    },
-}
+return require("telescope").register_extension({
+	exports = {
+		gitdirs = search_git_dirs,
+	},
+})
